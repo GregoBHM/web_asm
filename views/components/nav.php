b2ca393e39676ac818f071cceccfa6c00208d715
GregBHM <grehuanca@upt.pe>
2025-06-10 18:16:49 -0500

    v4.0

--- views/components/nav.php
+++ views/components/nav.php
@@ -1,19 +1,23 @@
 <?php if (isset($_SESSION['rol_id'])): ?>
     <div class="submenu-rol">
         <?php if ($_SESSION['rol_id'] === 2): ?>
-            <a href="<?= BASE_URL ?>/views/estudiante/clases.php">Mis Clases</a>
-            <a href="<?= BASE_URL ?>/views/estudiante/mentores.php">Mis Mentores</a>
-            <a href="<?= BASE_URL ?>/views/estudiante/calificaciones.php">Calificaciones</a>
+            <a href="<?= BASE_URL ?>/index.php?accion=clases">Mis Clases</a>
+            <a href="<?= BASE_URL ?>/index.php?accion=mentores">Mis Mentores</a>
+            <a href="<?= BASE_URL ?>/index.php?accion=calificaciones">Calificaciones</a>
 
         <?php elseif ($_SESSION['rol_id'] === 3): ?>
-            <a href="<?= BASE_URL ?>/views/dashboards/alumnos.php">Alumnos</a>
-            <a href="<?= BASE_URL ?>/views/dashboards/sesiones.php">Sesiones</a>
+            <a href="<?= BASE_URL ?>/index.php?accion=alumnos">Alumnos</a>
+            <a href="<?= BASE_URL ?>/index.php?accion=sesiones">Sesiones</a>
 
         <?php elseif ($_SESSION['rol_id'] === 4): ?>
-            <a href="<?= BASE_URL ?>/views/dashboards/usuarios.php">Gestión de Usuarios</a>
-            <a href="<?= BASE_URL ?>/views/dashboards/reportes.php">Reportes</a>
+            <a href="<?= BASE_URL ?>/index.php?accion=usuarios">Gestión de Usuarios</a>
+            <a href="<?= BASE_URL ?>/index.php?accion=reportes">Reportes</a>
         <?php endif; ?>
 
-        <a href="<?= BASE_URL ?>/views/usuario/perfil.php">Mi Perfil</a>
+        <?php if ($_SESSION['rol_id'] === 1): ?>
+            <a href="<?= BASE_URL ?>/index.php?accion=vincular">Vincularme a la UPT</a>
+        <?php endif; ?>
+
+        <a href="<?= BASE_URL ?>/index.php?accion=perfil">Mi Perfil</a>
     </div>
 <?php endif; ?>
